920b9350a3dde6a89d86a00b9c5eab06780e1c99
Sass-1 <sassielouis5@gmail.com>
2026-02-17 15:35:57 -0500

    Moving the fox

--- coin-collector/coin.py
+++ coin-collector/coin.py
@@ -14,21 +14,32 @@ coin = Actor("coin")
 coin.pos = 200, 200
 
 def draw():
-   screen.fill("green")
-   fox.draw()
-   coin.draw()
-   screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+    screen.fill("green")
+    fox.draw()
+    coin.draw()
+    screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+    
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Final Score: " + str(score), topleft=(10, 10), fontsize=60)
    
 def place_coin():
-   coin.x = randint(20, (WIDTH - 20))
-   coin.y = randint(20, (HEIGHT - 20))
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT - 20))
 
 def time_up():
-   global game_over
-   game_over = True
+    global game_over
+    game_over = True
 
 def update():
-   pass   
+    if keyboard.left:
+        fox.x = fox.x - 2
+    elif keyboard.right:
+        fox.x = fox.x + 2
+    elif keyboard.up:
+        fox.y = fox.y - 2
+    elif keyboard.down:
+        fox.y = fox.y + 2    
 
 clock.schedule(time_up, 7.0)
 place_coin()
