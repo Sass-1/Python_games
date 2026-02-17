d6c485d690fd045620e4e90fbfedf8b0c81b041a
Sass-1 <sassielouis5@gmail.com>
2026-02-16 22:21:20 -0500

    Upgrading the game

--- shoot_the_fruit/shoot.py
+++ shoot_the_fruit/shoot.py
@@ -1,6 +1,8 @@
 from random import randint
 import pgzrun
+count = 0
 apple = Actor("apple")
+
 
 def draw():
     screen.clear( )
@@ -11,13 +13,16 @@ def place_apple( ):
     apple.y = randint(10, 600)
     
 def on_mouse_down(pos):
+    global count
     if apple.collidepoint(pos):
         print("Good shot!")
+        count = count + 1
         place_apple()
     else:
         print("You missed!")
+        print (f"You've hit the fruit {count} times")
         quit()
- 
+
 place_apple()    
 
 pgzrun.go()
