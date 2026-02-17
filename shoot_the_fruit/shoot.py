bdbbedd262af41f592f2cd2790753344f4ca984d
Sass-1 <sassielouis5@gmail.com>
2026-02-16 21:56:17 -0500

    Adding some logic

--- shoot_the_fruit/shoot.py
+++ shoot_the_fruit/shoot.py
@@ -8,6 +8,15 @@ def draw():
 def place_apple( ):
     apple.x = 300
     apple.y = 200
+    
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot!")
+        place_apple()
+    else:
+        print("You missed!")
+        quit()
+ 
 place_apple()    
 
 pgzrun.go()
