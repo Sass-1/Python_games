ccc9045c1cae764c7efcad007e14941cb1d88856
Sass-1 <sassielouis5@gmail.com>
2026-02-17 07:03:03 -0500

    Add kiwi to shoot the fruit game

--- shoot_the_fruit/shoot.py
+++ shoot_the_fruit/shoot.py
@@ -1,28 +1,34 @@
 from random import randint
 import pgzrun
 count = 0
-apple = Actor("apple")
+fruits = ["kiwi", "apple"]
+
+target = Actor(fruits[0])
 
 
 def draw():
     screen.clear( )
-    apple.draw( )
+    target.draw( )
     
-def place_apple( ):
-    apple.x = randint(10, 800)
-    apple.y = randint(10, 600)
+def place_target( ):
+    
+    random_index = randint(0, len(fruits) - 1)
+    target.image = fruits[random_index]
+    
+    target.x = randint(50, 750)
+    target.y = randint(50, 550)
     
 def on_mouse_down(pos):
     global count
-    if apple.collidepoint(pos):
+    if target.collidepoint(pos):
         print("Good shot!")
         count = count + 1
-        place_apple()
+        place_target()
     else:
         print("You missed!")
         print (f"You've hit the fruit {count} times")
         quit()
 
-place_apple()    
+place_target()    
 
 pgzrun.go()
