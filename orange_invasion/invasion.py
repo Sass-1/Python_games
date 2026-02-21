edbcc7f139abc4d7551de9fbf8dd9ed37093dbd7
Sass-1 <sassielouis5@gmail.com>
2026-02-20 20:57:13 -0500

    Placing the actor cowboy

--- orange_invasion/invasion.py
+++ orange_invasion/invasion.py
@@ -1,3 +1,19 @@
 import pgzrun
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+WIDTH = 400
+HEIGHT = 400
+
+cowboy = Actor("cowboy")
+cowboy.pos = 200, 350
+
+bullet = Actor("bullet")
+bullet.active = False
+
+def draw ():
+    screen.clear()
+    cowboy.draw()
 
 pgzrun.go()
