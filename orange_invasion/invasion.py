6811c9904c28f3300275c598b5a316076431148d
Sass-1 <sassielouis5@gmail.com>
2026-02-21 21:22:39 -0500

    Moving the bullet

--- orange_invasion/invasion.py
+++ orange_invasion/invasion.py
@@ -10,7 +10,7 @@ HEIGHT = 400
 score = 0
 
 orange = Actor("orange")
-orange.pos = randint(20,350),0
+orange.pos = randint(10,350),0
     
 cowboy = Actor("cowboy")
 cowboy.pos = 200, 350
@@ -24,26 +24,40 @@ def draw ():
     cowboy.draw()
     orange.draw()
     screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
-    
+    if bullet.active:
+        bullet.draw()
     
 def update ():
-   ''' orange.y += 2
+    global score
+    orange.y += 0.5
     if orange.y > HEIGHT or orange.colliderect(cowboy):
-        quit ()'''
+        quit ()
     
     if keyboard.left:
         cowboy.x -= 2
     elif keyboard.right:
-        cowboy.x += 2
-    
+        cowboy.x += 2 
     if cowboy.x < 20 :
         cowboy.x = 20
     elif cowboy.x > 380 :
         cowboy.x = 380
+    
+    if bullet.active:
+        bullet.y -= 4
+        
+    if bullet.y < 0:
+        bullet.active = False
+    elif bullet.colliderect(orange):
+        score += 10
+        bullet.active = False
+        orange.pos = randint(10,350),0
 
 
-def on_mouse_down ():
-    pass
+def on_mouse_down (pos):
+    if not bullet.active:  
+        bullet.x = cowboy.x
+        bullet.y = cowboy.y -20
+        bullet.active = True
 
 
 pgzrun.go()
